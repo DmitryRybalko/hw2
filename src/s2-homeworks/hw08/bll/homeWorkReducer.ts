82f8ea0fea350fe3f4b3cc7c505f9400d80cbf3c
DmitryRybalko <dmitryrybalkodev@gmail.com>
2023-12-10 20:02:28 +0800

    8th homework is done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,31 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      const newState = [...state];
+      if (action.payload === "up") {
+        return newState.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      if (action.payload === "down") {
+        return newState.sort((a, b) => -1 * a.name.localeCompare(b.name));
+      } else {
+        return state;
+      }
     }
-}
+    case "check": {
+      return state.filter((u) => u.age >= 18);
+    }
+    default:
+      return state;
+  }
+};
