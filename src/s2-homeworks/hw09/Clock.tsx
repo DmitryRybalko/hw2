1416a3238014607219d667a73cc04495a4f4ff22
DmitryRybalko <dmitryrybalkodev@gmail.com>
2023-12-19 22:37:26 +0800

    10th homework is done

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -74,13 +74,11 @@ function Clock() {
 
   const stringTime =
     `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}` || <br />; // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-  const stringDate = "" || <br />; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+  const stringDate = `${formatDate(date)}` || <br />; // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
   // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
   const stringDay = daysOfWeek[date.getDay()] || <br />; // пишут студенты
-  const stringMonth = `${monthsOfYear[date.getMonth()]}, ${formatDate(
-    date
-  )}` || <br />; // пишут студенты
+  const stringMonth = `${monthsOfYear[date.getMonth()]}` || <br />; // пишут студенты
 
   return (
     <div className={s.clock}>
@@ -100,7 +98,7 @@ function Clock() {
         <div className={s.more}>
           {show ? (
             <>
-              <span id={"hw9-month"}>{stringMonth}</span>
+              <span id={"hw9-month"}>{stringMonth}</span>, {""}
               <span id={"hw9-date"}>{stringDate}</span>
             </>
           ) : (
